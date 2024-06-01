21d45fb0befdb8712635951743b2940d57fc0676
vplapps <68125352+vplapps@users.noreply.github.com>
2024-06-01 22:34:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -73,6 +73,7 @@ def delete_user(user_id):
     return redirect(url_for('users'))
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
     with app.app_context():
         db.create_all()
     app.run(debug=True)
